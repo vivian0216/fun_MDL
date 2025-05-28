257f8924f091a23e9cd46343230dd6c7435339db
shreyas <shreyaskalvankar@gmail.com>
2025-05-28 13:02:29 +0200

    update instructions

--- dvsgesture/smodels.py
+++ dvsgesture/smodels.py
@@ -1,16 +1,29 @@
 import torch
 import torch.nn as nn
-from spikingjelly.cext.neuron import MultiStepParametricLIFNode
-from spikingjelly.clock_driven import layer
+from spikingjelly.activation_based import layer, neuron
+
+
+class MultiStepParametricLIFNode(neuron.ParametricLIFNode):
+    def __init__(self, **kwargs):
+        super().__init__(step_mode="m", **kwargs)
+
 
 def conv3x3(in_channels, out_channels):
     return nn.Sequential(
         layer.SeqToANNContainer(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=3,
+                padding=1,
+                stride=1,
+                bias=False,
+            ),
             nn.BatchNorm2d(out_channels),
         ),
-        MultiStepParametricLIFNode(init_tau=2.0, detach_reset=True)
+        MultiStepParametricLIFNode(init_tau=2.0, detach_reset=True),
     )
+
 
 def conv1x1(in_channels, out_channels):
     return nn.Sequential(
@@ -18,8 +31,9 @@ def conv1x1(in_channels, out_channels):
             nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
             nn.BatchNorm2d(out_channels),
         ),
-        MultiStepParametricLIFNode(init_tau=2.0, detach_reset=True)
+        MultiStepParametricLIFNode(init_tau=2.0, detach_reset=True),
     )
+
 
 class SEWBlock(nn.Module):
     def __init__(self, in_channels, mid_channels, connect_f=None):
@@ -32,16 +46,17 @@ class SEWBlock(nn.Module):
 
     def forward(self, x: torch.Tensor):
         out = self.conv(x)
-        if self.connect_f == 'ADD':
+        if self.connect_f == "ADD":
             out += x
-        elif self.connect_f == 'AND':
+        elif self.connect_f == "AND":
             out *= x
-        elif self.connect_f == 'IAND':
-            out = x * (1. - out)
+        elif self.connect_f == "IAND":
+            out = x * (1.0 - out)
         else:
             raise NotImplementedError(self.connect_f)
 
         return out
+
 
 class PlainBlock(nn.Module):
     def __init__(self, in_channels, mid_channels):
@@ -54,14 +69,21 @@ class PlainBlock(nn.Module):
     def forward(self, x: torch.Tensor):
         return self.conv(x)
 
+
 class BasicBlock(nn.Module):
     def __init__(self, in_channels, mid_channels):
         super(BasicBlock, self).__init__()
         self.conv = nn.Sequential(
             conv3x3(in_channels, mid_channels),
-
             layer.SeqToANNContainer(
-                nn.Conv2d(mid_channels, in_channels, kernel_size=3, padding=1, stride=1, bias=False),
+                nn.Conv2d(
+                    mid_channels,
+                    in_channels,
+                    kernel_size=3,
+                    padding=1,
+                    stride=1,
+                    bias=False,
+                ),
                 nn.BatchNorm2d(in_channels),
             ),
         )
@@ -78,40 +100,39 @@ class ResNetN(nn.Module):
         conv = []
 
         for cfg_dict in layer_list:
-            channels = cfg_dict['channels']
+            channels = cfg_dict["channels"]
 
-            if 'mid_channels' in cfg_dict:
-                mid_channels = cfg_dict['mid_channels']
+            if "mid_channels" in cfg_dict:
+                mid_channels = cfg_dict["mid_channels"]
             else:
                 mid_channels = channels
 
             if in_channels != channels:
-                if cfg_dict['up_kernel_size'] == 3:
+                if cfg_dict["up_kernel_size"] == 3:
                     conv.append(conv3x3(in_channels, channels))
-                elif cfg_dict['up_kernel_size'] == 1:
+                elif cfg_dict["up_kernel_size"] == 1:
                     conv.append(conv1x1(in_channels, channels))
                 else:
                     raise NotImplementedError
 
             in_channels = channels
 
-
-            if 'num_blocks' in cfg_dict:
-                num_blocks = cfg_dict['num_blocks']
-                if cfg_dict['block_type'] == 'sew':
+            if "num_blocks" in cfg_dict:
+                num_blocks = cfg_dict["num_blocks"]
+                if cfg_dict["block_type"] == "sew":
                     for _ in range(num_blocks):
                         conv.append(SEWBlock(in_channels, mid_channels, connect_f))
-                elif cfg_dict['block_type'] == 'plain':
+                elif cfg_dict["block_type"] == "plain":
                     for _ in range(num_blocks):
                         conv.append(PlainBlock(in_channels, mid_channels))
-                elif cfg_dict['block_type'] == 'basic':
+                elif cfg_dict["block_type"] == "basic":
                     for _ in range(num_blocks):
                         conv.append(BasicBlock(in_channels, mid_channels))
                 else:
                     raise NotImplementedError
 
-            if 'k_pool' in cfg_dict:
-                k_pool = cfg_dict['k_pool']
+            if "k_pool" in cfg_dict:
+                k_pool = cfg_dict["k_pool"]
                 conv.append(layer.SeqToANNContainer(nn.MaxPool2d(k_pool, k_pool)))
 
         conv.append(nn.Flatten(2))
@@ -132,41 +153,191 @@ class ResNetN(nn.Module):
         x = self.conv(x)
         return self.out(x.mean(0))
 
+
 def SEWResNet(connect_f):
     layer_list = [
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'sew', 'k_pool': 2},
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "sew",
+            "k_pool": 2,
+        },
     ]
     num_classes = 11
     return ResNetN(layer_list, num_classes, connect_f)
 
+
 def PlainNet(*args, **kwargs):
     layer_list = [
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'plain', 'k_pool': 2},
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "plain",
+            "k_pool": 2,
+        },
     ]
     num_classes = 11
     return ResNetN(layer_list, num_classes)
 
+
 def SpikingResNet(*args, **kwargs):
     layer_list = [
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
-        {'channels': 32, 'up_kernel_size': 1, 'mid_channels': 32, 'num_blocks': 1, 'block_type': 'basic', 'k_pool': 2},
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
+        {
+            "channels": 32,
+            "up_kernel_size": 1,
+            "mid_channels": 32,
+            "num_blocks": 1,
+            "block_type": "basic",
+            "k_pool": 2,
+        },
     ]
     num_classes = 11
     return ResNetN(layer_list, num_classes)
